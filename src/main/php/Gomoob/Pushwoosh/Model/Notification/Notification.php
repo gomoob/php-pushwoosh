7ba938ac71ddc6c2888ebd75c15a9c6d97ab7f28
Baptiste Gaillard <baptiste.gaillard@gomoob.com>
2014-05-20 15:06:10 +0200

    Improves the Notification class.

--- src/main/php/Gomoob/Pushwoosh/Model/Notification/Notification.php
+++ src/main/php/Gomoob/Pushwoosh/Model/Notification/Notification.php
@@ -6,12 +6,20 @@
 namespace Gomoob\Pushwoosh\Model\Notification;
 
 use Gomoob\Pushwoosh\PushwooshException;
+
 /**
  * Class which represents a Pushwoosh notification.
  *
  * @author Baptiste GAILLARD (baptiste.gaillard@gomoob.com)
  */
 class Notification {
+
+	/**
+	 * An object which contains specific Pushwoosh notification informations for ADM (Amazon Device Messaging).
+	 *
+	 * @var \Gomoob\Pushwoosh\Model\Notification\ADM
+	 */
+	private $aDM;
 
 	/**
 	 * The object which contains specific Pushwoosh notification informations for Android (Google Cloud Messaging).
@@ -93,6 +101,32 @@ class Notification {
 
 	private $link;
 
+	public function getLink() {
+
+		return $this -> link;
+
+	}
+
+	public function setLink($link) {
+
+		$this -> link = $link;
+
+	}
+
+	/**
+	 * The object which contains specific Pushwoosh notification informations for Mac OS X.
+	 *
+	 * @var \Gomoob\Pushwoosh\Model\Notification\Mac
+	 */
+	private $mac;
+
+	/**
+	 * The object which contains specific Pushwoosh notification informations for Safari.
+	 *
+	 * @var \Gomoob\Pushwoosh\Model\Notification\Safari
+	 */
+	private $safari;
+
 	/**
 	 * The date when the message has to be sent, if a string is provided it must respect the following formats :
 	 * 	- 'now'				: To indicate the message has to be sent when "now".
@@ -105,6 +139,19 @@ class Notification {
 	private $sendDate = 'now';
 
 	private $ignoreUserTimezone;
+
+	public function isIgnoreUserTimezone() {
+
+		return $this -> ignoreUserTimezone;
+
+	}
+
+	public function setIgnoreUserTimezone($ignoreUserTimezone) {
+
+		$this -> ignoreUserTimezone = $ignoreUserTimezone;
+
+	}
+
 
 	/**
 	 * Optional parameter, can have the following values :
@@ -119,9 +166,45 @@ class Notification {
 	 */
 	private $minimizeLink;
 
+	/**
+	 * HTML page id (created from Application’s HTML Pages). Use this if you want to deliver additional HTML content to
+	 * the application or omit this parameter.
+	 *
+	 * @var int
+	 */
 	private $pageId;
 
-	private $plateforms;
+	/**
+	 *
+	 * @var unknown
+	 */
+	private $platforms;
+
+	public function getPlatforms() {
+
+		return $this -> platforms;
+
+	}
+
+	public function setPlatforms(array $platforms) {
+
+		$this -> platforms = $platforms;
+
+	}
+
+	/**
+	 * The object which contains specific Pushwoosh notification informations for WNS (Windows Notification Service).
+	 *
+	 * @var \Gomoob\Pushwoosh\Model\Notification\WNS
+	 */
+	private $wNS;
+
+	/**
+	 * The object which contains specific Pushwoosh notification informations for WP (Windows Phone).
+	 *
+	 * @var \Gomoob\Pushwoosh\Model\Notification\WP
+	 */
+	private $wP;
 
 	/**
 	 * Utility function used to create a new notification.
@@ -131,6 +214,18 @@ class Notification {
 	public static function create() {
 
 		return new Notification();
+
+	}
+
+	/**
+	 * Gets the object which contains specific Pushwoosh notification informations for ADM (Amazon Device Messaging).
+	 *
+	 * @return \Gomoob\Pushwoosh\Model\Notification\ADM the object which contains specific Pushwoosh notification
+	 *         informations for ADM (Amazon Device Messaging).
+	 */
+	public function getADM() {
+
+		return $this -> aDM;
 
 	}
 
@@ -231,6 +326,42 @@ class Notification {
 	}
 
 	/**
+	 * Gets the object which contains specific Pushwoosh notification informations for Mac OS X.
+	 *
+	 * @return \Gomoob\Pushwoosh\Model\Notification\Mac the object which contains specific Pushwoosh notification
+	 *         informations for Mac OS X.
+	 */
+	public function getMac() {
+
+		return $this -> mac;
+
+	}
+
+	/**
+	 * Gets the HTML page id (created from Application’s HTML Pages). Use this if you want to deliver additional HTML
+	 * content to the application or omit this parameter.
+	 *
+	 * @return int the HTML page id.
+	 */
+	public function getPageId() {
+
+		return $this -> pageId;
+
+	}
+
+	/**
+	 * Gets the object which contains specific Pushwoosh notification informations for Safari.
+	 *
+	 * @return \Gomoob\Pushwoosh\Model\Notification\Safari the object which contains specific Pushwoosh notification
+	 *         informations for Safari.
+	 */
+	public function getSafari() {
+
+		return $this -> safari;
+
+	}
+
+	/**
 	 * Gets the date when the message has to be sent, if a string is provided it must respect the following formats :
 	 * 	- 'now'				: To indicate the message has to be sent when "now".
 	 *  - 'YYYY-MM-DD HH:mm	: Specify your own send date.
@@ -241,6 +372,43 @@ class Notification {
 	public function getSendDate() {
 
 		return $this -> sendDate;
+
+	}
+
+	/**
+	 * Gets the object which contains specific Pushwoosh notification informations for WNS (Windows Notification
+	 * Service).
+	 *
+	 * @return \Gomoob\Pushwoosh\Model\Notification\WNS the object which contains specific Pushwoosh notification
+	 *         informations for WNS (Windows Notification Service).
+	 */
+	public function getWNS() {
+
+		return $this -> wNS;
+
+	}
+
+	/**
+	 * Gets the object which contains specific Pushwoosh notification informations for WP (Windows Phone).
+	 *
+	 * @return \Gomoob\Pushwoosh\Model\Notification\WP the object which contains specific Pushwoosh notification
+	 *         informations for WP (Windows Phone).
+	 */
+	public function getWP() {
+
+		return $this -> wP;
+
+	}
+
+	/**
+	 * Sets the object which contains specific Pushwoosh notification informations for ADM (Amazon Device Messaging).
+	 *
+	 * @param \Gomoob\Pushwoosh\Model\Notification\ADM $aDM the object which contains specific Pushwoosh notification
+	 *        informations for ADM (Amazon Device Messaging).
+	 */
+	public function setADM(ADM $aDM) {
+
+		$this -> aDM = $aDM;
 
 	}
 
@@ -342,6 +510,42 @@ class Notification {
 	}
 
 	/**
+	 * Sets the object which contains specific Pushwoosh notification informations for Mac OS X.
+	 *
+	 * @param \Gomoob\Pushwoosh\Model\Notification\Mac $mac the object which contains specific Pushwoosh notification
+	 *        informations for Mac OS X.
+	 */
+	public function setMac(Mac $mac) {
+
+		$this -> mac = $mac;
+
+	}
+
+	/**
+	 * Sets the HTML page id (created from Application’s HTML Pages). Use this if you want to deliver additional HTML
+	 * content to the application or omit this parameter.
+	 *
+	 * @param int $pageId the HTML page id to set1.
+	 */
+	public function setPageId($pageId) {
+
+		$this -> pageId = $pageId;
+
+	}
+
+	/**
+	 * Sets the object which contains specific Pushwoosh notification informations for Safari.
+	 *
+	 * @param \Gomoob\Pushwoosh\Model\Notification\Safari $safari the object which contains specific Pushwoosh
+	 *        notification informations for Safari.
+	 */
+	public function setSafari(Safari $safari) {
+
+		$this -> safari = $safari;
+
+	}
+
+	/**
 	 * Sets the date when the message has to be sent, if a string is provided it must respect the following formats :
 	 * 	- 'now'				: To indicate the message has to be sent when "now".
 	 *  - 'YYYY-MM-DD HH:mm	: Specify your own send date.
@@ -365,6 +569,31 @@ class Notification {
 		}
 
 		throw new PushwooshException('Invalid send date provided !');
+
+	}
+
+	/**
+	 * Sets the object which contains specific Pushwoosh notification informations for WNS (Windows Notification
+	 * Service).
+	 *
+	 * @param \Gomoob\Pushwoosh\Model\Notification\WNS $wNS the object which contains specific Pushwoosh notification
+	 *        informations for WNS (Windows Notification Service).
+	 */
+	public function setWNS($wNS) {
+
+		$this -> wNS = $wNS;
+
+	}
+
+	/**
+	 * Sets the object which contains specific Pushwoosh notification informations for WP (Windows Phone).
+	 *
+	 * @param \Gomoob\Pushwoosh\Model\Notification\WP $wP the object which contains specific Pushwoosh notification
+	 *        informations for WP (Windows Phone).
+	 */
+	public function setWP(WP $wP) {
+
+		$this -> wP = $wP;
 
 	}
 
