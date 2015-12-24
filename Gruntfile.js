ae658317c123877a76c950d7fcbaf566cae0eca6
Baptiste GAILLARD <baptiste.gaillard@gomoob.com>
2015-12-25 00:20:30 +0100

    Come back to grunt-phpdocumentor.

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,6 +35,23 @@ module.exports = function(grunt) {
                 }
                 
             }, /* Copy task */
+            
+            /**
+             * PHPDocumentor Task.
+             */
+            phpdocumentor : {
+                            
+                options : {
+                    directory : 'src/main/php',
+                    target : 'build/reports/phpdocumentor'
+                },       
+                
+                /**
+                 * Target used to generate the PHP documentation of the project.
+                 */
+                generate : {}
+
+            }, /* PHPDocumentor Task */
 
             /**
              * PHPUnit Task.
@@ -118,7 +135,7 @@ module.exports = function(grunt) {
                 
                 phpdocumentor : {
                     command : function() {
-                        return 'php vendor/phpdocumentor/phpdocumentor/bin/phpdoc --target=build/reports/phpdocumentor --directory=src/main/php';
+                        return 'phpdoc --target=build/reports/phpdocumentor --directory=src/main/php';
                     }
                 },
                 
