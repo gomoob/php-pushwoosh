f2763c3604d6df144042351b61d6f8757ee8015b
anibalsanchez <anibal.sanchez@extly.com>
2017-06-28 20:06:56 +0200

    setImage('Image')

--- src/test/php/Gomoob/Pushwoosh/Model/Notification/NotificationTest.php
+++ src/test/php/Gomoob/Pushwoosh/Model/Notification/NotificationTest.php
@@ -541,6 +541,7 @@ class NotificationTest extends TestCase
                     ->setGcmTtl(3600)
                     ->setIcon('icon')
                     ->setTitle('Title')
+                    ->setImage('Image')
             )
             ->setIOS(
                 IOS::create()
@@ -666,6 +667,7 @@ class NotificationTest extends TestCase
         $this->assertSame(3600, $array['chrome_gcm_ttl']);
         $this->assertSame('icon', $array['chrome_icon']);
         $this->assertSame('Title', $array['chrome_title']);
+        $this->assertSame('Image', $array['chrome_image']);
 
         // Test IOS parameters
         $this->assertSame(1, $array['apns_trim_content']);
